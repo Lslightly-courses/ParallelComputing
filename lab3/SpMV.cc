96756117b2187658335b7059d55253465c59429f
liqingwei <lqw332664203@mail.ustc.edu.cn>
2022-05-18 16:38:15 +0800

    执行时间、计算比例、通信比例统计

--- lab3/SpMV.cc
+++ lab3/SpMV.cc
@@ -38,78 +38,96 @@ int countNNZ(CSR &s, int i, int j) {
     return s.ptr[j] - s.ptr[i];
 }
 
+typedef struct Statistic {
+    double total_time;
+    double collect_per;
+    double cal_per;
+}Statistic;
+
 #ifndef SERIAL
-double MPICSRSpMV(int argc, char **argv, int nprocs, int myrank) {
+Statistic MPICSRSpMV(int argc, char **argv, int nprocs, int myrank) {
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
     double start, end, duration, total_start, total_end;
-    double t_cal, t_gather;
+    double t_cal, t_collect;
+    double tag_res = 1;
     CSR s = input();
     initVec(s);
     int col_num = s.col_num;
     int row_num = s.row_num;
     int r_procs = s.row_num/nprocs;
     double *local_res;
-    double *res = (double*)malloc(sizeof(double)*(s.row_num));
+    double *res;
     int *idx = s.idx+s.ptr[myrank*r_procs];
     double *val = s.val+s.ptr[myrank*r_procs];
     int *ptr = s.ptr+myrank*r_procs;
-    int left = s.row_num - myrank*r_procs;
+    int left = row_num - myrank*r_procs;
     MPI_Barrier(MPI_COMM_WORLD);
     total_start = MPI_Wtime();
     START_TIMING
     if (myrank == nprocs-1) {  //  master
-        int nnz = countNNZ(s, myrank*r_procs, s.row_num);
+        res = (double*)malloc(sizeof(double)*(s.row_num));
+        int nnz = countNNZ(s, myrank*r_procs, row_num);
         local_res = (double *)malloc(sizeof(double)*left);
         CSR local_s = {left, col_num, nnz, ptr, idx, val, s.vec, local_res};
         CSRSpMV(local_s, ptr[0]);
-        freeAll(s);
+        for (int i = (nprocs-1)*r_procs; i < row_num; i++) {
+            res[i] = local_res[i-(nprocs-1)*r_procs];
+        }
     } else {
         int nnz = countNNZ(s, myrank*r_procs, (myrank+1)*r_procs);
         local_res = (double *)malloc(sizeof(double)*r_procs);
         CSR local_s = {r_procs, col_num, nnz, ptr, idx, val, s.vec, local_res};
         CSRSpMV(local_s, ptr[0]);
-        freeAll(s);
+        MPI_Send(local_res, r_procs, MPI_DOUBLE, nprocs-1, tag_res, MPI_COMM_WORLD);
+        free(local_res);
     }
+    freeAll(s);
     GET_TIME
     t_cal = duration;
     START_TIMING
-    MPI_Gather(local_res, r_procs, MPI_DOUBLE, res, r_procs, MPI_DOUBLE, nprocs-1, MPI_COMM_WORLD);
     if (myrank == nprocs-1) {
-        GET_TIME
-        t_gather = duration;
-        for (int i = nprocs*r_procs; i < row_num; i++) {
-            res[i] = local_res[i-myrank*r_procs];
+        MPI_Status status;
+        for (int i = 0; i < nprocs-1; i++) {
+            MPI_Recv(res+i*r_procs, r_procs, MPI_DOUBLE, i, tag_res, MPI_COMM_WORLD, &status);
         }
+        GET_TIME
+        t_collect = duration;
         total_end = MPI_Wtime();
         double total_duration = total_end - total_start;
-        fprintf(stderr, "cal%%:%f gather%%:%f\n", t_cal/total_duration, t_gather/total_duration);
         printResult(res, row_num);
-        return total_end - total_start;
+        free(res);
+        free(local_res);
+        return {total_duration, t_collect/total_duration, t_cal/total_duration};
     }
-    return 0;
+    return {};
 }
 #endif
 
 int main(int argc, char **argv)
 {
+    int LOOPS = stoi(argv[1]);
     #ifndef SERIAL
-    MPI_Init(&argc, &argv);
-    int myrank;
-    double duration = 0;
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-    int nprocs = stoi(argv[1]);
+        MPI_Init(&argc, &argv);
+        int myrank;
+        double duration = 0;
+        double cal_per, collect_per = 0;
+        MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+        int nprocs = stoi(argv[2]);
     #endif
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < LOOPS; i++) {
         #ifndef SERIAL
-        duration += MPICSRSpMV(argc, argv, nprocs, myrank);
+            auto tmp_result = MPICSRSpMV(argc, argv, nprocs, myrank);
+            duration += tmp_result.total_time;
+            cal_per += tmp_result.cal_per;
+            collect_per += tmp_result.collect_per;
         #else
-        serialSpMV();
+            serialSpMV();
         #endif
     }
     #ifndef SERIAL
-    if (myrank == nprocs-1)
-        cerr << duration << "s" << endl;
-    MPI_Finalize();
+        if (myrank == nprocs-1)
+            fprintf(stderr, "%f %f%% %f%%\n", duration/LOOPS, cal_per/LOOPS*100, collect_per/LOOPS*100);
+        MPI_Finalize();
     #endif
     return 0;
 }
--- lab3/compare.py
+++ lab3/compare.py
@@ -1,35 +1,29 @@
 import subprocess
 from os import system
-import time
+
 
 system('make all')
-start = time.time()
-serial_exec = subprocess.run('./serialSpMV', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-end = time.time()
-duration = end-start
-print('串行执行时间 {}ms'.format(duration*1000))
-print(serial_exec.stderr.decode(encoding='utf-8'))
-with open('perf.txt', 'w') as f:
-    f.write('{} {}\n'.format('算法', '执行时间/ms'))
-    f.write('{} {}\n'.format('串行', duration*1000))
+LOOPS = 3
+serial_exec = subprocess.run('./serialSpMV {0}'.format(LOOPS), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
 serial_result = serial_exec.stdout.decode(encoding='utf-8')
 
-for procs in range(1, 12):
+list_perf = []
+for procs in range(1, 13):
     print(procs)
-    start = time.time()
-    mpi_exec = subprocess.run('mpiexec -n {0} ./parallelSpMV {0}'.format(procs), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-    end = time.time()
-    duration = end-start
-    # print('MPI执行时间 {}ms'.format(duration*1000))
+    mpi_exec = subprocess.run('mpiexec -n {1} ./parallelSpMV {0} {1}'.format(LOOPS, procs), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
     mpi_result = mpi_exec.stdout.decode(encoding='utf-8')
-    print(mpi_exec.stderr.decode(encoding='utf-8'))
+    perf_str = mpi_exec.stderr.decode(encoding='utf-8').removesuffix('\n')
+    print(perf_str)
+    list_perf.append(perf_str.split(' '))
     if mpi_result != serial_result:
         print('结果不同')
     else:
         print('结果相同')
-    with open('serial.txt', 'w') as f:
-        f.write(serial_result)
-    with open('mpi.txt', 'w') as f:
-        f.write(mpi_result)
-    with open('perf.txt', 'a') as f:
-        f.write('{} {}\n'.format(procs, duration*1000))
+with open('serial.txt', 'w') as f:
+    f.write(serial_result)
+with open('mpi.txt', 'w') as f:
+    f.write(mpi_result)
+with open('perf.txt', 'w') as f:
+    f.write('进程数 总执行时间/ms 计算比例 通信比例\n')
+    for procs_1, perf in enumerate(list_perf):
+        f.write('{} {} {} {}\n'.format(procs_1+1, perf[0], perf[1], perf[2]))
